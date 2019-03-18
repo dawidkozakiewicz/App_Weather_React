6d358af997a442e26aa5660da4adc59b6aef3612
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-03-18 23:19:13 +0100

    build state

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,68 @@
 import React, { Component } from 'react';
-import Form from './Form'
-import Result from './Result'
-
+import Form from './Form';
+import Result from './Result';
+const APIKey = '55bae99201abf05122511e2802d3fc8b';
 
 class App extends Component {
+	state = {
+		value: '',
+		date: '',
+		city: '',
+		sunrise: '',
+		sunset: '',
+		temp: '',
+		pressure: '',
+		wind: '',
+		err: false
+	};
 
-  state = {
-    value: '',
-    date: '',
-    city: '',
-    sunrise: '',
-    sunset: '',
-    temp: '',
-    pressure: '',
-    wind: '',
-    err: '',
-}
+	handleCitySubmit = (e) => {
+		e.preventDefault();
+		console.log('potwierdzony formularz');
+		const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state
+			.value},pl&APPID=${APIKey}&units=metric`;
+		fetch(API)
+			.then((response) => {
+				if (response.ok) {
+					return response;
+				}
+				throw Error('Nie udało się');
+			})
+			.then((response) => response.json())
+			.then((data) => {
+        const time = new Date().toLocaleDateString()
+				this.setState({
+          err: false,
+          date: time,
+			    city: this.state.value,
+			    sunrise: data.sys.sunrise,
+			    sunset: data.sys.sunset,
+			    temp: data.main.temp,
+			    pressure: data.main.pressure,
+			    wind: data.wind,
+				});
+			})
+			.catch((err) => {
+			  this.setState({
+			    err: true,
+			    
+			  });
+			});
+	};
+	handleInputChange = (e) => {
+		this.setState({
+			value: e.target.value
+		});
+	};
 
-  handleCitySubmit = (e) => {
-    e.preventDefault()
-    console.log('potwierdzony formularz')
-    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value},pl&APPID=55bae99201abf05122511e2802d3fc8b`
-    fetch(API)
-    .then(response => {
-      if(response.ok) {
-        return response
-      }
-      throw Error("Nie udało się")
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-  }
-  handleInputChange = (e) => {
-    this.setState({
-      value: e.target.value
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-       <Form 
-       value={this.state.value}
-      change={this.handleInputChange}
-      submit={this.handleCitySubmit}
-       />
-       <Result />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Form value={this.state.value} change={this.handleInputChange} submit={this.handleCitySubmit} />
+				<Result error={this.state.err}/>
+			</div>
+		);
+	}
 }
 
 export default App;
